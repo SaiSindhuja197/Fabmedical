6a5760073cd1dccd119cc495af281e45665ca3d6
SaiSindhuja197 <saisindhujakarra@gmail.com>
2023-01-06 15:21:06 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,11 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("31ae3b5b-8668-49ea-bff5-bd3c433acc6a");
+appInsights.start();
+
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
